cb079aff595270230c450a4dea6540e33325786d
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-12 03:19:37 -0300

    Create Routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} exact />
+    </Switch>
+  );
+};
+
+export default Routes;
